6c4cddcc3dce0fc906f86862573fa056227a9aa8
Flavio Toro B. <torentk@live.com>
2025-09-02 11:25:29 -0400

    v2
    mejora en manejo de vistas y diccionarios, se agrega fondo y se manejan errores de referencia

--- js/script.js
+++ js/script.js
@@ -1,104 +1,119 @@
 const DICCIONARIO = {
-    "High": {
+    "1_grade": {
         "Normal Round": [
-            "Accommodation", "Advertisements", "Amazement", "Appreciation", "Appropriation",
-            "Arctic", "Assessing", "Assignment", "Attempt", "Audience",
-            "Awareness", "Backlash", "Backstage", "Baggage", "Biography",
-            "Brainstorm", "Breathtaking", "Campaign", "Carriage", "Celebration",
-            "Centimetre", "Chandelier", "Characters", "Clothing", "Commemorate",
-            "Composers", "Customs", "Disappear", "Disappearance", "Environment",
-            "Equivalent", "Fashion", "Festival", "Follower", "Garment",
-            "Grasshopper", "Graveyard", "Headline", "Heritage", "Influence",
-            "Labour", "Landfill", "Launched", "Malware", "Manipulate",
-            "Measure", "Misfortune", "Misinformation", "Musical", "Mysterious",
-            "Newspaper", "Overcome", "Overtime", "Overwhelming", "Passage",
-            "Performance", "Persuade", "Pharmacist", "Phishing", "Pickpocket",
-            "Platform", "Population", "Privacy", "Prosperous", "Relative",
-            "Report", "Restoration", "Review", "Seized", "Settlement",
-            "Shipping", "Shutdown", "Shutters", "Skyscraper", "Slogans",
-            "Speckled", "Struggle", "Surround", "Sustainability", "Sustainable",
-            "Themselves", "Threatened", "Triumph", "Troublesome", "Underground",
-            "Upcycle", "Volunteer", "Voyage", "Whisper", "Witnesses"
+            "leg", "ant", "small", "got", "pet",
+            "back", "Max", "pen", "dad", "pot",
+            "bag", "lip", "tent", "fat", "quick",
+            "big", "hop", "up", "Ron", "rock",
+            "comb", "sit", "wig", "lock", "sick",
+            "can", "not", "way", "fish", "see",
+            "rat", "pants", "wet", "help", "sad",
+            "fits", "pat", "wing", "hot", "song",
+            "bad", "mat", "use", "fox", "ring",
+            "did", "man", "van", "kit", "pen"
         ],
         "Extra Round": [
-            "Adventure", "Animated", "Animator", "Award", "Background",
-            "Benefits", "Entertainment", "Enthusiastic", "Famous", "Filmmaker",
-            "Initiatives", "Impressive", "Paragraph", "Recommend", "Recycling",
-            "Resolution", "Setting", "Soundtrack", "Ancient", "Builders",
-            "Culprit", "Distraction", "Exceptional", "Friendship", "Guilty",
-            "Harmony", "Inseparable", "Jealous", "Knight", "Library",
-            "Metropolitan", "Neighbourhood", "Obedient", "Pickpocket", "Qualificate",
-            "Research", "Suspect", "Thief", "Urgently", "Victorious",
-            "Worldwide", "Xylophone", "Yield", "Zeppelin"
-        ]
-    }, "Secondary": {
-        "Normal Round": [
-            "Acrobat", "Airplane", "Animal", "Arcade", "Astronaut",
-            "Aunt", "Australian", "Ballerina", "Beautiful", "Blonde",
-            "Bookstore", "British", "Brother", "Brown", "Carpenter",
-            "Cartoon", "Cheese", "Children", "Chilean", "Circus",
-            "Clown", "Comedy", "Condor", "Costume", "Curly",
-            "Director", "Documentary", "Drama", "Ear", "Elephant",
-            "French", "Friendly", "German", "Glasses", "Guitar",
-            "Harmonica", "Hero", "Horror", "Juggler", "Keys",
-            "Mexican", "Microphone", "Monkey", "Mother", "Mouse",
-            "Parade", "Parents", "Scene", "Script", "Sharks",
-            "Small", "Square", "Strawberries", "Subway", "Talented",
-            "Taxi", "Thumb", "Tickets", "Tiger", "Tightrope",
-            "Tooth", "Tooth", "Uncle", "Vegetarian", "Violín",
-            "Walker", "Watch", "Western", "World", "Writer"
-        ],
-        "Extra Round": [
-            "Astronomer", "Brown", "Cardigan", "Crater", "Dress",
-            "Eye", "Finger", "Glasses", "Hoodie", "Jupiter",
-            "Meteor", "Mouth", "Square", "Straight", "Sweatshirt",
-            "Tights", "Universe", "Wavy"
-        ]
-    }, "Primary": {
-        "Normal Round": [
-            "Grandma", "Brother", "Listen", "Touch", "Legs",
-            "Eyes", "Firefighter", "Nurse", "Sloth", "Small",
-            "History", "English", "Bookstore", "Next", "Hospital",
-            "Friday", "Saturday", "Name", "Walk", "Count",
-            "Short", "Slow", "Pool", "Lake", "Twenty",
-            "Parents", "Forty", "Uncle", "March", "Date",
-            "Calendar", "Noon", "Eat", "Early", "Lunch",
-            "Soccer", "Golf", "Tennis", "Football", "Walk",
-            "Running", "Picnic", "Playing", "Shorts", "Sleep",
-            "Jeans", "Swim", "Sneakers", "Hat", "Gloves",
-            "Boots", "Scarf", "Comb", "Soap", "Shampoo",
-            "Toothbrush", "Clinic", "Clarinet", "Cymbals", "Piano",
-            "Camel", "Ostrich", "Wing", "Desert", "Shoulder",
-            "Ankle", "Cold", "Fever", "Necklace", "Drumsticks",
-            "Wallet", "Keyboard", "Subway", "Bookstore", "Helicopter",
-            "Arcade", "Scientist", "Tickets", "Writer", "Costumes",
-            "Aquarium", "Famous", "Elevator", "Concert", "Strong",
-            "Quiet", "Weak", "Noisy", "Interesting", "Talented"
 
+        ]
+    }, "2_grade": {
+        "Normal Round": [
+            "feathers", "afraid", "trunk", "cake", "lost",
+            "again", "friends", "sing", "always", "games",
+            "think", "dance", "monkey", "baby", "her",
+            "Tuesday", "dresses", "nothing", "ball", "hurry",
+            "slower", "club", "lucky", "anyone", "growing",
+            "spring", "chair", "morning", "after", "heavy",
+            "under", "ducks", "our", "bigger", "Jack",
+            "water", "egg", "nose", "bird", "window",
+            "ear", "saddest", "body", "July", "everyone",
+            "sang", "wish", "kisses", "fastest", "seed"
         ],
         "Extra Round": [
-            "Colorful", "Amazing", "Ringmaster", "Clown", "Unicyclist",
-            "Juggler", "Cameraman", "Actors", "Script", "Producer",
-            "Cardigan", "Astronaut", "Clothes", "Tights", "Comedy",
-            "Grumpy", "Western", "Silly", "Saturn", "Farthest",
-            "Earth", "Nearest", "Smartwatch", "Expensive", "Powerful",
-            "Camera"
+
+        ]
+    }, "3_grade": {
+        "Normal Round": [
+            "experiment", "chase", "morning", "state", "world",
+            "country", "enjoy", "machines", "miss", "safe",
+            "shape", "woman", "bunch", "early", "listen",
+            "rescue", "whale", "build", "dust", "spider",
+            "nests", "someone", "wonderful", "cover", "excited",
+            "lunch", "smile", "talking", "climb", "everything",
+            "lemonade", "late", "people", "amphibian", "dropping",
+            "hungry", "pineapple", "vapor", "brave", "drawing",
+            "horse", "vote", "born", "danger", "Wednesday",
+            "mammals", "teacher", "amazing", "different", "fire"
+        ],
+        "Extra Round": [
+        ]
+    }, "4_grade": {
+        "Normal Round": [
+            "Sunday", "motion", "thousand", "Christmas", "gardener",
+            "minute", "soap", "boring", "fridge", "worried",
+            "million", "sequence", "butterfly", "flippers", "uniform",
+            "mechanic", "quarter", "because", "envelope", "spotted",
+            "together", "March", "private", "bicycle", "enough",
+            "worth", "leave", "passenger", "geography", "path",
+            "backyard", "dynamic", "glove", "hatch", "penguin",
+            "actor", "dessert", "twice", "August", "daughter",
+            "tooth", "great", "parents", "actually", "chess",
+            "wasn’t", "through", "November", "aunts", "character"
+        ],
+        "Extra Round": [
+        ]
+    }, "5_grade": {
+        "Normal Round": [
+            "newspaper", "shopping", "camera", "explore", "waitress",
+            "nowhere", "screen", "business", "electricity", "young",
+            "kingdom", "relaxing", "breakfast", "dollar", "visited",
+            "join", "quick", "before", "didn’t", "umbrella",
+            "juice", "quietly", "autumn", "choose", "tomorrow",
+            "journey", "people", "assistant", "correct", "traffic",
+            "introduce", "potatoes", "August", "crowded", "summer",
+            "healthy", "photography", "address", "college", "strange",
+            "hobby", "passport", "anybody", "cinnamon", "sometimes",
+            "exciting", "message", "accident", "chemist", "sentence"
+        ],
+        "Extra Round": [
+        ]
+    }, "6_grade": {
+        "Normal Round": [
+            "noisy", "Switzerland", "career", "guide", "windscreen",
+            "medicine", "special", "campsite", "glasses", "wilderness",
+            "luggage", "spaghetti", "building", "favourite", "yourself",
+            "kilometre", "sweater", "busy", "fantastic", "women",
+            "journalist", "somewhere", "brought", "foreign", "wedding",
+            "impossible", "surprised", "blanket", "factory", "wasn’t",
+            "hosts", "telephone", "quiet", "scratch", "amazing",
+            "downstairs", "vegetable", "heavy", "couldn’t", "affirmative",
+            "download", "thunderstorm", "healthy", "passenger", "advance",
+            "comfortable", "thousand", "hairdryer", "opposite", "account"
+        ],
+        "Extra Round": [
         ]
     }
 
 };
 
-const ELEMENTOS = {
-    texto: document.getElementById('texto'),
-    numero: document.getElementById('numero'),
-    dropdownMenuButton: document.getElementById('dropdownMenuButton'),
-    icono: document.getElementById('icono')
+let ELEMENTOS = {};
+
+function refreshELEMENTOS() {
+    ELEMENTOS = {
+        texto: document.getElementById('texto'),
+        numero: document.getElementById('numero'),
+        dropdownMenuButton: document.getElementById('dropdownMenuButton'),
+        icono: document.getElementById('icono')
+    };
+}
+const GRADES_MAP = {
+    "1_grade": "1st Grade",
+    "2_grade": "2nd Grade",
+    "3_grade": "3rd Grade",
+    "4_grade": "4th Grade",
+    "5_grade": "5th Grade",
+    "6_grade": "6th Grade"
 };
 
-
-
-// Puedes seguir aplicando el tamaño de fuente que quieras para otros elementos
-ELEMENTOS.numero.style.fontSize = '2.9em';
 
 
 let intervalo;
@@ -128,10 +143,6 @@ function girarNumeros(grade) {
     ELEMENTOS.numero.style.fontSize = '10em';
     const totalPalabrasRonda = DICCIONARIO[grade][valorElemento].length;
 
-    const DIFICULTAD = {
-        'First Round': 'Easy', 'Second Round': 'Medium', 'Third Round': 'Hard'
-    }[valorElemento];
-
     resetearAnimacion();
 
     intervalo = setInterval(() => {
@@ -158,10 +169,8 @@ function girarNumeros(grade) {
 
         ELEMENTOS.numero.textContent = finalNum;
         palabrasSeleccionadas.push(palabraAsociada);
+        iniciarAnimacionEscritura(palabraAsociada, ELEMENTOS.numero, "");
 
-        if (["Secondary", "Primary","High"].includes(grade)) {
-            iniciarAnimacionEscritura(palabraAsociada, ELEMENTOS.numero, "");
-        } 
     }, 2000);
 }
 
@@ -197,8 +206,8 @@ function iniciarAnimacionEscritura(palabra, numeroDiv) {
             searchIcon.setAttribute('alt', 'enabled');
             const contenedorTabla = document.querySelector('[alt="contenedor_tabla"]');
             contenedorTabla.style = '';
-                agregarPalabraATabla2(numeroDiv.textContent, palabra);
-            
+            agregarPalabraATabla2(numeroDiv.textContent, palabra);
+
 
         }
     }
@@ -265,3 +274,59 @@ function selectRonda(ronda) {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+//view
+function loadBanner() {
+    fetch("views/banner.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar banner.html");
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById("container-body").innerHTML = html;
+        })
+        .catch(error => console.error("Error cargando banner:", error));
+}
+
+
+function loadContent(level) {
+    fetch("views/baseview.html")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar baseview.html");
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById("container-body").innerHTML = html;
+            refreshELEMENTOS();
+
+            // actualizar título
+            const titulo = document.getElementById("title");
+            if (titulo && GRADES_MAP[level]) {
+                titulo.innerHTML = `<img src="img/bee.png" width="60" height="60" alt=""> ${GRADES_MAP[level]}`;
+            }
+
+            // estado global
+            window.STATE = { grade: level, name: GRADES_MAP[level] };
+
+            const dropdownBtn = document.getElementById("dropdownMenuButton");
+            if (dropdownBtn) {
+                dropdownBtn.innerHTML = `<i class="fa fa-cog fa-spin fa-1x fa-fw fa-sm text-white-50"></i> Normal Round`;
+                dropdownBtn.setAttribute("value", "Normal Round");
+            }
+            let resetbtn = document.getElementById("resetbutton");
+            if (resetbtn) {
+                resetbtn.setAttribute("onclick", `loadContent('${level}')`);
+            }
+            const icono = document.getElementById("icono");
+            if (icono) {
+                icono.setAttribute("onclick", `girarNumeros('${level}')`);
+            }
+        })
+        .catch(error => console.error("Error cargando vista:", error));
+}
+
+
